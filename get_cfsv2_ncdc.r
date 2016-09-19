f319fea508ad80bebd827538b09c2126433bf58b
Dan Broman <daniel.p.broman@gmail.com>
2016-09-19 14:51:10 -0600

    Update get_cfsv2_ncdc.r

--- get_cfsv2_ncdc.r
+++ get_cfsv2_ncdc.r
@@ -1,17 +1,18 @@
 ###########################################
-# get_cfsv2.r
+# get_cfsv2_ncdc.r
 # pulls cfsv2 forecasts from NCDC archive
 # subsets to gbm and africa domains
 # pulls out precip. surface temp, winds, and latent
 # heat flux
-# pulling all runs, 0-18z init times, out to 60 days (1440 hours)
 ###########################################
+
 start_time = Sys.time()
+
+## load libraries
 library(stringr)
 library(dplyr)
 library(data.table)
 library(lubridate)
-# at inithour = 00z - 01 goes out 9mon 02-04 go out 1 season, inithour = 06-18z - 01 goes out 9mon, 02-04 out 45 days
 
 date2str = function(dte){
 	#dte - POSIXct date 
@@ -28,63 +29,80 @@ savdir_scratch = ''
 savdir_gbm = ''
 savdir_africa = ''
 
-start_date = '2015-01-01'
-end_date = '2015-12-31'
-runstr = c('01') # which ensemble members are desired - in NCDC archive only '01' member available
-inithours = c(0, 6, 12, 18)
-fcst_lead = 1440 # archive goes out a maximum of 9 months
+dir_scratch = ''
+dir_gbm = ''
+dir_africa = ''
 
-## setup run
-timeinit = as.POSIXct(format(end_date, tz = 'utc'))
-timeinitcheck = as.POSIXct(format(start_date, tz = 'utc'))
-fcsthours = seq(from = 0, to = fcst_lead, by = 6) 
+fcst_lead_sel = 1440
+time_sel_start = as.POSIXct('2011-04-01', tz = 'utc')
+time_sel_end = as.POSIXct('2011-12-31 18:00', tz = 'utc')
 
-## check files for last 2 days and fill in if needed
-timeinitcheck_ls = seq(from = round(timeinitcheck, 'days'), to = timeinit, by = '6 hour')
-check_tbl = data.table()
-for(i in 1:length(timeinitcheck_ls)){
-	check_tbl_temp = data.table(init_date = timeinitcheck_ls[i], fcst_date = timeinitcheck_ls[i] + hours(fcsthours_45day), run = rep(runstr, each = length(fcsthours)))
-	check_tbl = bind_rows(check_tbl, check_tbl_temp)
-}
-check_tbl = check_tbl %>% mutate(filename = paste0(date2str(fcst_date), '_', run, '_', date2str(init_date), '.grb2'))
-check_tbl = check_tbl %>% filter(init_date != fcst_date)
-nfiles = nrow(check_tbl)
+ncores_sel = 6
 
-cl = makeCluster(7)
-registerDoParallel(cl)
-foreach (i = 1:nfiles) %dopar% {
-# for(i in 1:nfiles){
+## setup
+setwd(dir_scratch)
+
+time_init_list = seq(from = time_sel_start, to = time_sel_end, by = '6 hour')
+fcst_lead_list = seq(from = 6, to = fcst_lead_sel, by = 6)
+nfcstlead = length(fcst_lead_list)
+time_dt = data.table(time_init = rep(time_init_list, each = nfcstlead), fcst_lead = fcst_lead_list) %>% mutate(time_fcst = time_init + hours(fcst_lead))
+ntimes = nrow(time_dt)
+
+tempvar_list = rep(letters, ceiling(ntimes / 26))
+
+## download function
+get_cfs_grb = function(time_init_sel, time_fcst_sel, tempvar){
 	require(data.table)
 	require(dplyr)
 	require(lubridate)
 	require(stringr)
 	require(tidyr)
-	file_temp = check_tbl[i, ]$filename
-	initdatestr = date2str(check_tbl[i, ]$init_date)
-	inityr = substr(initdatestr, 1, 4)
-	inityrmon = substr(initdatestr, 1, 6)
-	initdatefilestr = substr(initdatestr, 1, 8)
+
+	yearinit = year(time_init_sel)
+	monthinit = str_pad(month(time_init_sel), 2, pad = "0")
+	dayinit = str_pad(day(time_init_sel), 2, pad = "0")
+	hourinit = str_pad(hour(time_init_sel), 2, pad = "0")
+
+	initdatefilestr = paste0(yearinit, monthinit, dayinit)
+	initdatestr = paste0(yearinit, monthinit, daynow, hourinit)
+	
+	yeafcstr = year(time_fcst_sel)
+	monthfcst = str_pad(month(time_fcst_sel), 2, pad = "0")
+	dayfcst = str_pad(day(time_fcst_sel), 2, pad = "0")
+	hourfcst = str_pad(hour(time_fcst_sel), 2, pad = "0")
+
 	fcstdatestr = date2str(check_tbl[i, ]$fcst_date)
-	runstr_sel = check_tbl[i, ]$run
+
 	url = paste0('http://nomads.ncdc.noaa.gov/modeldata/cfsv2_forecast_6-hourly_9mon_flxf/', inityr, '/', inityrmon, '/', initdatefilestr, '/', initdatestr, '/flxf', fcstdatestr, '.01.', initdatestr, '.grb2')
-	if(file.exists(paste0(savdir_gbm, file_temp)) == F){
-		try(system(paste0('wget ', url, ' -O ', savdir_scratch, file_temp)))
-		if(file.exists(paste0(savdir_scratch, file_temp)) == T){
-			system(paste0('wgrib2 ', savdir_scratch, file_temp, ' -match ":(TMP:2 m above ground|PRATE|CPRAT|LHTFL|UGRD:10 m above ground|VGRD:10 m above ground):" -small_grib  73:98 22:32 ',  savdir_gbm, file_temp))		
-
+	
+	destfile_gbm = paste0(dir_gbm, fcstdatestr, '_', '01', '_', initdatestr ,'.grb2') 
+	destfile_africa = paste0(dir_africa, fcstdatestr, '_', '01', '_', initdatestr ,'.grb2') 
+	
+	#downloads and subsets (if needed)
+	if(file.exists(destfile_gbm) == F){
+		download.file(url, paste0('temp_', tempvar, '.grb2'), mode = 'wb')
+		
+		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match ":(TMP:2 m above ground|PRATE|CPRAT|LHTFL|UGRD:10 m above ground|VGRD:10 m above ground):" -small_grib ", paste(lims_lon_gbm, collapse = ':'), " ", paste(lims_lat_gbm, collapse = ':'), " ", destfile_gbm), ignore.stdout = T, ignore.stderr = T)
+		
+		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match '", fcst_match_list, "' -g2clib 0 -small_grib ", paste(lims_lon_gbm, collapse = ':'), " ", paste(lims_lat_gbm, collapse = ':'), " ", destfile_gbm), ignore.stdout = T, ignore.stderr = T)
+		if(file.exists(destfile_africa) == F){
+			system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match ":(TMP:2 m above ground|PRATE|CPRAT|LHTFL|UGRD:10 m above ground|VGRD:10 m above ground):" -small_grib ", paste(lims_lon_africa, collapse = ':'), " ", paste(lims_lat_africa, collapse = ':'), " ", destfile_africa), ignore.stdout = T, ignore.stderr = T)
 		}
 	}
-
-	if(file.exists(paste0(savdir_africa, file_temp)) == F){
-		if(file.exists(paste0(savdir_scratch, file_temp)) == F){
-			try(system(paste0('wget ', url, ' -O ', savdir_scratch, file_temp)))
-		}
-		if(file.exists(paste0(savdir_scratch, file_temp)) == T){
-			
-			system(paste0('wgrib2 ', savdir_scratch, file_temp, ' -match ":(TMP:2 m above ground|PRATE|CPRAT|LHTFL|UGRD:10 m above ground|VGRD:10 m above ground):" -small_grib  -20:55 -40:40 ',  savdir_africa, file_temp))
-		}
+	if(file.exists(destfile_africa) == F){
+		download.file(url, paste0('temp_', tempvar, '.grb2'), mode = 'wb')
+		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match ":(TMP:2 m above ground|PRATE|CPRAT|LHTFL|UGRD:10 m above ground|VGRD:10 m above ground):" -small_grib ", paste(lims_lon_africa, collapse = ':'), " ", paste(lims_lat_africa, collapse = ':'), " ", destfile_africa), ignore.stdout = T, ignore.stderr = T)
 	}
-	system(paste0('rm ', savdir_scratch, file_temp))
+}
+
+## call function
+cl = makeCluster(ncores_sel)
+registerDoParallel(cl)
+foreach (i = 1:ntimes) %dopar% {
+	time_init_sel = time_dt$time_init[i]
+	time_fcst_sel = time_dt$time_fcst[i]
+	tempvar_sel = tempvar_list[i]
+	try(get_cfs_grb(time_init_sel, fcst_lead_sel, tempvar_sel))
 }
 stopCluster(cl)
 Sys.time() - start_time
