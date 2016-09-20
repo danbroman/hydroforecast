319086b0f166d91e17385b4e2f6674380b24ce21
Dan Broman <daniel.p.broman@gmail.com>
2016-09-20 14:55:28 -0600

    Update cfsv2_ts_ncdc_sqlite.r
    removed unneeded lines of code. added code for africa domain as well

--- cfsv2_ts_ncdc_sqlite.r
+++ cfsv2_ts_ncdc_sqlite.r
@@ -16,10 +16,7 @@ library(sqldf)
 
 dir_scratch = '/d1/dbroman/projects/cfsv2/scratch/'
 dir_gbm = '/d1/dbroman/projects/cfsv2/grib2/gbm/'
-dir_africa = '/d1/dbroman/projects/cfsv2/grib2/africa/'
-
 dir_gbm_proc = '/d1/dbroman/projects/cfsv2/rdata/gbm/'
-dir_africa_proc = '/d1/dbroman/projectscfsv2/rdata/africa/'
 
 setwd(dir_gbm_proc)
 db = dbConnect(SQLite(), dbname = 'gbm_cfsv2.sqlite')
@@ -30,6 +27,27 @@ nfiles_gbm = length(gbm_file_list)
 for(i in 1:nfiles_gbm){
   gbm_file_sel = paste0(dir_gbm, gbm_file_list[i])
 	system(paste0("wgrib2 ", gbm_file_sel, " -csv temp.csv"))
+	tryCatch({
+	fcst_dt_temp = fread('temp.csv') %>% setnames(c('date_init', 'date_fcst', 'var', 'level', 'lon', 'lat', 'value'))
+	})
+	var_temp = unique(fcst_dt_temp$var)
+  dbWriteTable(conn = db, name = var_temp, value = fcst_dt_temp, row.names = F, append = T)
+}
+
+
+dir_scratch = '/d1/dbroman/projects/cfsv2/scratch/'
+dir_africa = '/d1/dbroman/projects/cfsv2/grib2/africa/'
+dir_africa_proc = '/d1/dbroman/projects/cfsv2/rdata/africa/'
+
+setwd(dir_africa_proc)
+db = dbConnect(SQLite(), dbname = 'africa_cfsv2.sqlite')
+
+africa_file_list = list.files(dir_africa, pattern = '*.grb2')
+nfiles_africa = length(africa_file_list)
+
+for(i in 1:nfiles_africa){
+  africa_file_sel = paste0(dir_africa, africa_file_list[i])
+	system(paste0("wgrib2 ", africa_file_sel, " -csv temp.csv"))
 	tryCatch({
 	fcst_dt_temp = fread('temp.csv') %>% setnames(c('date_init', 'date_fcst', 'var', 'level', 'lon', 'lat', 'value'))
 	})
