3f3fcabe95515232aac8842ac2333bc9abf307d2
Dan Broman <daniel.p.broman@gmail.com>
2016-09-29 12:40:20 -0600

    Update get_cfsv2_ts_ncdc.r

--- get_cfsv2_ts_ncdc.r
+++ get_cfsv2_ts_ncdc.r
@@ -1,7 +1,7 @@
 ###########################################
 # get_cfsv2_ts_ncdc.r
 # pulls cfsv2 forecasts from NCDC timeseries archive
-# subsets to gbm and africa domains
+# subsets to ext and africa domains
 # pulls out specified forecast variables
 ###########################################
 
@@ -18,13 +18,10 @@ library(foreach)
 
 ## user inputs
 dir_scratch = ''
-dir_gbm = ''
-dir_africa = ''
+dir_ext = ''
 
-lims_lon_gbm = c(73, 98)
-lims_lat_gbm = c(22, 32)
-lims_lon_africa = c(-20, 55)
-lims_lat_africa = c(-40, 40)
+lims_lon_ext = c(73, 98)
+lims_lat_ext = c(22, 32)
 
 fcst_lead_sel = 1440
 time_sel_start = as.POSIXct('2011-04-01', tz = 'utc')
@@ -66,36 +63,21 @@ get_cfs_ts_grb = function(var, time_init_sel, fcst_lead = 1440, tempvar){
 	
 	url = paste0(urlhead, yearinit, '/', inityrmon, '/', initdatefilestr, '/', initdatestr, '/', var, '.', '01', '.', initdatestr, '.daily.grb2') 
 	
-	destfile_gbm = paste0(dir_gbm, var, '.', initdatestr, '.', '01', '.grb2') 
-	destfile_africa = paste0(dir_africa, var, '.', initdatestr, '.', '01', '.grb2') 
+	destfile_ext = paste0(dir_ext, var, '.', initdatestr, '.', '01', '.grb2') 
 	
 	#checks forecast lead against selected and removes file if too small
-	if(file.exists(destfile_gbm) == T){
-		file_meta = data.table(raw = system(paste('wgrib2', destfile_gbm, '-ftime'), intern = T)) %>% separate(raw, sep = c(':'), into = c('id', 'ref', 'hour')) %>% separate(hour, sep = ' ', into = c('hour', 'lab1', 'lab2'))
+	if(file.exists(destfile_ext) == T){
+		file_meta = data.table(raw = system(paste('wgrib2', destfile_ext, '-ftime'), intern = T)) %>% separate(raw, sep = c(':'), into = c('id', 'ref', 'hour')) %>% separate(hour, sep = ' ', into = c('hour', 'lab1', 'lab2'))
 		file_max_hour = max(as.numeric(file_meta$hour))
 		if(file_max_hour < fcst_lead){
-			system(paste("rm", destfile_gbm))
+			system(paste("rm", destfile_ext))
 		}
 	}
-	if(file.exists(destfile_africa) == T){
-		file_meta = data.table(raw = system(paste('wgrib2', destfile_africa, '-ftime'), intern = T)) %>% separate(raw, sep = c(':'), into = c('id', 'ref', 'hour')) %>% separate(hour, sep = ' ', into = c('hour', 'lab1', 'lab2'))
-		file_max_hour = max(as.numeric(file_meta$hour))
-		if(file_max_hour < fcst_lead){
-			system(paste("rm", destfile_africa))
-		}
-	}
-
+	
 	#downloads and subsets (if needed)
-	if(file.exists(destfile_gbm) == F){
+	if(file.exists(destfile_ext) == F){
 		download.file(url, paste0('temp_', tempvar, '.grb2'), mode = 'wb')
-		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match '", fcst_match_list, "' -g2clib 0 -small_grib ", paste(lims_lon_gbm, collapse = ':'), " ", paste(lims_lat_gbm, collapse = ':'), " ", destfile_gbm), ignore.stdout = T, ignore.stderr = T)
-		if(file.exists(destfile_africa) == F){
-			system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match '", fcst_match_list, "' -g2clib 0 -small_grib ", paste(lims_lon_africa, collapse = ':'), " ", paste(lims_lat_africa, collapse = ':'), " ", destfile_africa), ignore.stdout = T, ignore.stderr = T)
-		}
-	}
-	if(file.exists(destfile_africa) == F){
-		download.file(url, paste0('temp_', tempvar, '.grb2'), mode = 'wb')
-		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match '", fcst_match_list, "' -g2clib 0 -small_grib ", paste(lims_lon_africa, collapse = ':'), " ", paste(lims_lat_africa, collapse = ':'), " ", destfile_africa), ignore.stdout = T, ignore.stderr = T)
+		system(paste0('wgrib2 ', 'temp_', tempvar, '.grb2'," -match '", fcst_match_list, "' -g2clib 0 -small_grib ", paste(lims_lon_ext, collapse = ':'), " ", paste(lims_lat_ext, collapse = ':'), " ", destfile_ext), ignore.stdout = T, ignore.stderr = T)
 	}
 }
 
@@ -109,5 +91,6 @@ foreach (i = 1:ntimes) %dopar% {
 }
 stopCluster(cl)
 Sys.time() - start_time
-system(paste("find", dir_gbm, "-size -1k -delete"))
-system(paste("find", dir_africa, "-size -1k -delete"))
+
+#removes small files...if download didn't complete etc.
+system(paste("find", dir_ext, "-size -1k -delete"))
